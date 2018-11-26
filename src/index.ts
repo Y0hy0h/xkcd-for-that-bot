806424869bbe8b74d889e6b2b942716d883e9593
Y0hy0h <Y0hy0h@users.noreply.github.com>
2018-11-26 13:14:36 +0100

    Add dev mode

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import * as Express from 'express';
 import * as path from 'path';
 import * as bodyParser from 'body-parser';
-import { setupBot, setupWebhook } from './elmegram.js';
+import { setupBot, startPolling, setupWebhook } from './elmegram.js';
 const Bot = require('./bot.js')
 
 startServer(Express())
@@ -12,15 +12,23 @@ async function startServer(app: Express) {
   // Static website
   app.use(Express.static(path.join(__dirname, '/public/')))
 
-  const { token, handleUpdate } = await setup();
-  const webhookUrl = getWebhookUrl(token);
-  await setupWebhook(token, webhookUrl);
-  app.use(`/bot/${token}`, async (req, res, next) => {
-    console.log("\nReceived update:");
-    console.log(req.body);
-    handleUpdate(req.body);
-    res.sendStatus(200);
-  });
+  const unverifiedToken = getToken();
+  console.log(process.env.NODE_ENV)
+  if (process.env.NODE_ENV && process.env.NODE_ENV == 'dev') {
+    console.log('Development mode, starting to poll.')
+    startPolling(unverifiedToken, Bot);
+  } else {
+    console.log('Starting to listen to webhooks.')
+    const { token, handleUpdate } = await setupBot(unverifiedToken, Bot);
+    const webhookUrl = getWebhookUrl(token);
+    await setupWebhook(token, webhookUrl);
+    app.use(`/bot/${token}`, async (req, res, next) => {
+      console.log("\nReceived update:");
+      console.log(req.body);
+      handleUpdate(req.body);
+      res.sendStatus(200);
+    });
+  }
 
   const listener = app.listen(process.env.PORT, function () {
     console.log('Your app is listening on port ' + listener.address().port)
@@ -33,8 +41,8 @@ function getWebhookUrl(token: string): string {
   return `${host}/bot/${token}`;
 }
 
-export async function setup() {
+export function getToken(): string {
   const tokenName = 'TELEGRAM_TOKEN';
   const unverifiedToken = process.env[tokenName];
-  return setupBot(unverifiedToken, Bot)
+  return unverifiedToken;
 }
