5a7861c053f2e1a6f23ca32bdec2d64ef63d7686
Y0hy0h <Y0hy0h@users.noreply.github.com>
2018-11-26 01:31:56 +0100

    Use webhooks

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import * as Express from 'express';
 import * as path from 'path';
-
-import 'elmegram.js';
+import { setupBot, setupWebhook } from './elmegram.js';
+const Bot = require('./bot.js')
 
 startServer(Express())
 
@@ -9,9 +9,25 @@ async function startServer(app: Express) {
   // Static website
   app.use(Express.static(path.join(__dirname, '/public/')))
 
+  const { token, handleUpdate } = await setup();
+  const webhookUrl = getWebhookUrl(token);
+  await setupWebhook(token, webhookUrl);
+  app.use(`/bot/${token}`, handleUpdate)
+
   const listener = app.listen(process.env.PORT, function () {
     console.log('Your app is listening on port ' + listener.address().port)
   })
+}
 
-  await import('./startBot.js');
+function getWebhookUrl(token: string): string {
+  const domain = process.env.PROJECT_DOMAIN
+  const host = `${domain}.glitch.me`
+  const port = 8443
+  return `${host}:${port}/bot/${token}`;
+}
+
+export async function setup() {
+  const tokenName = 'TELEGRAM_TOKEN';
+  const unverifiedToken = process.env[tokenName];
+  return setupBot(unverifiedToken, Bot)
 }
