57b253bf71f4e05d052b9c5cdc5585560334d16d
Y0hy0h <Y0hy0h@users.noreply.github.com>
2018-11-26 01:54:45 +0100

    Fix misconfigured handler

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,26 @@
 import * as Express from 'express';
 import * as path from 'path';
+import * as bodyParser from 'body-parser';
 import { setupBot, setupWebhook } from './elmegram.js';
 const Bot = require('./bot.js')
 
 startServer(Express())
 
 async function startServer(app: Express) {
+  app.use(bodyParser.json());
+
   // Static website
   app.use(Express.static(path.join(__dirname, '/public/')))
 
   const { token, handleUpdate } = await setup();
   const webhookUrl = getWebhookUrl(token);
   await setupWebhook(token, webhookUrl);
-  app.use(`/bot/${token}`, handleUpdate)
+  app.use(`/bot/${token}`, async (req, res, next) => {
+    console.log("\nReceived update:");
+    console.log(req.body);
+    handleUpdate(req.body);
+    res.sendStatus(200);
+  });
 
   const listener = app.listen(process.env.PORT, function () {
     console.log('Your app is listening on port ' + listener.address().port)
